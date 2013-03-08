9520462e9ebedbbf617018e4329d53951737c0f7
khoa pham <p.khoa89@yahoo.com>
2013-03-08 00:31:44 -0600

    Created physician and patient

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HW4::Application.config.secret_token = 'fc30a7d431ca2b81c249cb17031a5be2c6940842ef87f016700f522d4f526aa5c652d7f89e2f2e6d19b8f45fcb1b5a511907bc11597be49686cccb837c3b26b7'
